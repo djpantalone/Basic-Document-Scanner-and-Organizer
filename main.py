2b17b79fc86078a8f7822cd8c75c45e333a8454b
William <william.jennings73@gmail.com>
2023-11-26 12:22:44 -0600

    Add terminal-based image upload functionality.

---
+++ main.py
@@ -0,0 +1,10 @@
+from upload_image import upload_image
+
+def main():
+    
+    upload_image()
+    # Here you can add more functions that you'll develop later
+    # For example, image preprocessing, OCR, etc.
+
+if __name__ == "__main__":
+    main()
---
+++ upload_image.py
@@ -0,0 +1,20 @@
+import os
+from PIL import Image
+
+def upload_image():
+    while True:
+        file_path = input("Enter the path to your image file: ")
+
+        if os.path.isfile(file_path):
+            try:
+                with Image.open(file_path) as img:
+                    print(f"Successfully uploaded {file_path}")
+                    break
+                
+            except IOError:
+                print("The file is not a valid image. Please try again.")
+        else:
+            print("File not found. Please enter a valid file path.")
+
+if __name__ == "__main__":
+    upload_image()
